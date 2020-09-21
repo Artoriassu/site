3a2cb286b5f8ebc266cd406a99e85e13c3477c87
Artoriassu <artoriasu@mail.ru>
2020-09-21 12:36:44 +0300

    Updates

---
+++ precache-manifest.84018bd290b6bfda926cac4add3f3b00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1c31bf02a722f4baebba2aa1d15e2e",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "75d24ed3dbfb405a43b0",
+    "url": "/site/static/css/main.a47f76fe.chunk.css"
+  },
+  {
+    "revision": "e40e1ce256905849903e",
+    "url": "/site/static/js/2.43170ae9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/site/static/js/2.43170ae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d24ed3dbfb405a43b0",
+    "url": "/site/static/js/main.b82c9c78.chunk.js"
+  },
+  {
+    "revision": "01f2061489b59b0c4c22",
+    "url": "/site/static/js/runtime-main.ac1a9a55.js"
+  },
+  {
+    "revision": "1181204bc0ef21c53bdd85ad29782e62",
+    "url": "/site/static/media/circles.1181204b.svg"
+  },
+  {
+    "revision": "365120d5c058fd590228adcf645fca9a",
+    "url": "/site/static/media/f1.365120d5.jpeg"
+  }
+]);
